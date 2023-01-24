56ba26da1b411fa8d19dac6896356bf11e4e3c36
TretyakovKonstantin <spb_unex@mail.ru>
2023-01-24 19:06:14 +0300

    Урок 1. Выполнено задание №2

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,24 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        List<Person> list = new LinkedList<>(List.of(
+        System.out.println("Задание №1");
+        List<Person> listPerson = new LinkedList<>(List.of(
                 new Person(4),
                 new Person(7),
                 new Person(2),
                 new Person(15),
                 new Person(9)));
 
-        findMinMax(list.stream(), new SortByPerson(), (personMin, personMax) -> {
+        findMinMax(listPerson.stream(), new SortByPerson(), (personMin, personMax) -> {
             System.out.println("Min: " + personMin.getAge());
             System.out.println("Max: " + personMax.getAge());
         });
 
+        System.out.println("Задание №2");
+        List<Integer> listInteger = new LinkedList<>(List.of(1, 4, 5, 3, 8, 6, 7, 9));
+        System.out.println("список - " + listInteger);
+        List<Integer> newListInteger = listInteger.stream().filter(i -> i % 2 == 0).toList();
+        System.out.println("количество четных - " + newListInteger.size());
     }
 
     public static void findMinMax(Stream<Person> stream,
